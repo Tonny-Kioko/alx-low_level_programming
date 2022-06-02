b0264b1edfbbc4e5b4694c13736c2ccc28bc086e
Tonny <anthonykioko@gmail.com>
2022-06-02 01:40:33 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
-#include <stdio.h> 
+#include <stdio.h>
+/**
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-	printf("with proper grammar, but the coutcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
