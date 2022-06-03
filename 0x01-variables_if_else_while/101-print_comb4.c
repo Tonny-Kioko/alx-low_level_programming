5591e7d1ec6c29c3f10c5439a6c319567fac13f2
Tonny <anthonykioko@gmail.com>
2022-06-03 13:20:06 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,20 +6,21 @@
  */
 int main(void)
 {
-		int i, j, k;
+	int i, j, k;
 
 	for (i = 48; i < 58; i++)
 	{
-		for (j = i; j <	58; j++)
+		for (j = i; j < 58; j++)
 		{
 			for (k = j; k < 58; k++)
+			{
 				if (i == j || j == k || i == k)
 				{
 					continue;
 				}
 				putchar(i);
 				putchar(j);
-				putchar (k);
+				putchar(k);
 
 				if (i == 55 && j == 56 && k == 57)
 				{
@@ -30,6 +31,7 @@ int main(void)
 					putchar(',');
 					putchar(' ');
 				}
+			}
 		}
 	}
 	putchar('\n');
