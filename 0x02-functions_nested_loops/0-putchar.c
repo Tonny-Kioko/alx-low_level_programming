83d029f3cda599148a6032704dcaf24ff2bcb5cc
Tonny <anthonykioko@gmail.com>
2022-06-07 00:15:26 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+
+/**
+ * main - entry point to the program
+ * to print _putchar
+ * return - to exit the program
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
