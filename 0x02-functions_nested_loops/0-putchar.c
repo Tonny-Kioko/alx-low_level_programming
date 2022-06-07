bd5bd2b75832fa9fb418749992012904af266fb4
Tonny <anthonykioko@gmail.com>
2022-06-07 03:05:49 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
-
 /**
- * main - entry point to the program
- * to print _putchar
- * return - to exit the program
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
 
 	return (0);
 }
--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,7 +3,6 @@
 /**
  * main - prints the n times table
  * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
  * Return: Always 0
  */
 void print_times_table(int n)
