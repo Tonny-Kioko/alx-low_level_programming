fd4b6e0211ef6a0c652788e9d6648f875ffb1b23
Tonny <anthonykioko@gmail.com>
2022-06-09 04:40:42 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,14 +10,14 @@ int _isupper(int c)
 	char uppercase = 'A';
 	int isupper = 0;
 
-	for (; uppercase <= 'Z'; uppercase++)
-	{
-		if (c == uppercase)
+		for (; uppercase <= 'Z'; uppercase++)
 		{
-			isupper = 1;
-			break;
+			if (c == uppercase)
+			{
+				isupper = 1;
+				break;
+			}
 		}
-	}
 
-	return (isupper);
+		return (isupper);
 }
