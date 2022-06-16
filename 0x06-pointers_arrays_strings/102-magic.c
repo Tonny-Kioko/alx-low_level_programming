04d4300d338a65fc4f8bc809b98a640683d70658
Tonny <anthonykioko@gmail.com>
2022-06-16 01:54:49 -0700

    102-maig.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'this code should print magic numbers'
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
@@ -15,7 +21,7 @@ p = &n;
  * - you are not allowed to modify p
  * - only one statement
  * - you are not allowed to code anything else than this line of code
-*/
+ */
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
