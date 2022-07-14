5e1c76770d024100430aa97d86534f958103392f
Tonny <anthonykioko@gmail.com>
2022-07-14 13:21:22 -0700

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,15 +1,19 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stddef.h>
+
 /**
- * free_list - frees a linked list
- * @head: the beginning of the list
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
  */
+
 void free_list(list_t *head)
 {
-	if (head == NULL)
-		return;
-	ree_list(head->next);
-	free(head->str);
-	free(head);
+	list_t *current;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
 }
