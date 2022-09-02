f5feb685e9c4c4d06624e796311402c174924ac4
Tonny <anthonykioko@gmail.com>
2022-09-02 13:59:44 -0700

    corrections and updates

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 /**
- *  * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
- *   * @head: A pointer to the head of the dlistint_t list.
- *    * @n: The integer for the new node to contain.
- *     *
- *      * Return: If the function fails - NULL.
- *       *         Otherwise - the address of the new node.
- *        */
+ * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
+ * @head: A pointer to the head of the dlistint_t list.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
+ */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new, *last;
--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- *  * sum_dlistint - Sums all the data of a dlistint_t list.
- *   * @head: The head of the dlistint_t list.
- *    *
- *     * Return: The sum of all the data.
- *      */
+ * sum_dlistint - Sums all the data of a dlistint_t list.
+ * @head: The head of the dlistint_t list.
+ *
+ * Return: The sum of all the data.
+ */
 int sum_dlistint(dlistint_t *head)
 {
 	int sum = 0;
