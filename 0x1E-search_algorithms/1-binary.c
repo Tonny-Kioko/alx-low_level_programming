6cf9485366b45dc3e3246c3516136d4e914423ff
Tonny-Kioko <anthonykioko5525@gmail.com>
2023-01-09 13:09:30 +0300

    Binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,3 +1,21 @@
+Skip to content
+Endale24
+/
+alx-low_level_programming
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+More
+alx-low_level_programming/0x1E-search_algorithms/1-binary.c
+@Endale24
+Endale24 Add files via upload
+ History
+ 1 contributor
+61 lines (47 sloc)  1.15 KB
 #include "search_algos.h"
 
 /**
@@ -20,4 +38,57 @@ int recursive_search(int *array, size_t size, int value)
 
 	printf("Searching in array");
 
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
 
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+
+	if (value == array[half])
+		return ((int)half);
+
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+
+	half++;
+
+	return (recursive_search(array + half, size - half, value) + half);
+}
+
+/**
+ * binary_search - calls to binary_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int index;
+
+	index = recursive_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
+		return (-1);
+
+	return (index);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+alx-low_level_programming/1-binary.c at master · Endale24/alx-low_level_programming
